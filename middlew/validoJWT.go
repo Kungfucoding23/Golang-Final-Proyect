b5cf0f1aafd749fa28621efb29b9b0950411bc67
Alejandro <alesantin012@hotmail.com>
2021-01-21 11:43:05 -0300

    EndPoint VerPerfil

---
+++ middlew/validoJWT.go
@@ -0,0 +1,21 @@
+package middlew
+
+/*
+	Este middleware chequea la validez del JWT
+*/
+import (
+	"net/http"
+
+	"github.com/Kungfucoding23/Golang-Final-Proyect/routers"
+)
+
+func ValidoJWT(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, _, _, err := routers.ProcesoToken(r.Header.Get("Authorization"))
+		if err != nil {
+			http.Error(w, "Error en el token"+err.Error(), http.StatusBadRequest)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
---
+++ models/claim.go
@@ -0,0 +1,17 @@
+package models
+
+/*
+En esta estructura procesamos el JWT que viene del Login, lo vamos a desencriptar de alguna manera dentro del modelo que es un JSON. Ese modelo lo llamamos Claim
+JWT es un estandar abierto que define un metodo compacto y autocontenido para encapsular y compartir aserciones (claims) sobre una entidad (subject) de manera segura entre distintas partes mediante el uso de objetos JSON.
+*/
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*Claim es la estructura usada para procesar el JWT */
+type Claim struct {
+	Email string             `json:"email"`
+	ID    primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	jwt.StandardClaims
+}
