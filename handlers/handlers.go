7bb4c55e98de2473bb4ff7f22f7dbc4d9b8f96e1
Alejandro <alesantin012@hotmail.com>
2021-01-21 11:45:32 -0300

    GET

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,7 +18,7 @@ func Handlers() {
 	// por cada EndPoint vamos a tener un renglon que permita manejar la funcion correspondiente
 	router.HandleFunc("/registro", middlew.CheckDB(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.CheckDB(routers.Login)).Methods("POST")
-	router.HandleFunc("/verperfil", middlew.CheckDB(middlew.ValidoJWT(routers.VerPerfil))).Methods("POST")
+	router.HandleFunc("/verperfil", middlew.CheckDB(middlew.ValidoJWT(routers.VerPerfil))).Methods("GET")
 	// Getenv recupera el valor de la variable de entorno nombrada por la clave.
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
